064080a8b668d3b0ca925559358cfea351ef5a63
Junior <jose.a.c.junior@outlook.com>
2021-05-22 21:52:40 -0300

    Adicionado funçoes no control e esboço da tela cadastra funcionario

--- src/boundary/Login.java
+++ src/boundary/Login.java
@@ -1,6 +1,10 @@
 package boundary;
 
+import control.Funcionarios;
+import entity.Funcionario;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -11,32 +15,66 @@ import javafx.stage.Stage;
 
 
 public class Login extends Application {
+    private Funcionarios controlFuncionarios = new Funcionarios();
+    Button btnEntrar = new Button("Entrar");
+    Button btnCadastrar = new Button("Cadastrar");
+
+    TextField txtUsuario = new TextField();
+    TextField txtSenha = new TextField();
+
+
 
     @Override
     public void start(Stage stage) throws Exception {
+
+
         Pane painel = new Pane();
         Scene scn = new Scene(painel, 250, 300);
+
+
 
         Label lblUsusario = new Label("Usuário");
         lblUsusario.relocate(60,40);
         Label lblSenha = new Label("Senha");
         lblSenha.relocate(60, 90);
 
-        TextField txtUsuario = new TextField();
+
         txtUsuario.relocate(50,60);
-        TextField txtSenha = new TextField();
+
         txtSenha.relocate(50,110);
 
-        Button btnEntrar = new Button("Entrar");
         btnEntrar.relocate(90,150);
-        Button btnCadastrar = new Button("Cadastrar");
         btnCadastrar.relocate(80,190);
+
         painel.getChildren().addAll(lblUsusario, lblSenha, txtUsuario, txtSenha, btnEntrar, btnCadastrar);
 
-        stage.setScene(scn);
-        stage.setTitle("S-Parking");
-        stage.show();
+        btnEntrar.setOnAction((e)->{
+                controlFuncionarios.loginFuncionario(txtUsuario.getText(),txtSenha.getText());
+        });
+
+        btnCadastrar.setOnAction((e)-> {
+
+            controlFuncionarios.cadastrar(boudaryToEntity());
+        });
+
+       stage.setScene(scn);
+       stage.setTitle("S-Parking");
+       stage.show();
     }
+
+    //Isso é so para testar a primeira tela, sera necessario criar uma tela de cadastro de funcionarios
+        public Funcionario boudaryToEntity()    {
+                    Funcionario f = new Funcionario();
+                    f.setUsuario(txtUsuario.getText());
+                    f.setSenha(txtSenha.getText());//Busca de usuario e senha ja funcionando.
+                    return f;
+        }//descobrir como utilizar a segunda tela(cadastroFuncionario)
+
+        public void entityToBoundary(Funcionario f){  //ainda nao tem utilidade para essa tela.
+            if (f !=null){
+            }
+        }
+
         public static void main(String[] args) {Application.launch(args);}
 
     }
---
+++ src/boundary/cadastroFuncionario.java
@@ -0,0 +1,69 @@
+package boundary;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class cadastroFuncionario extends Application {
+    TextField txtNome = new TextField("Nome");
+    TextField txtCPF = new TextField("CPF");
+    TextField txtTelefone = new TextField("Telefone");
+    TextField txtEndereco = new TextField("endereço");
+    TextField txtUsuario = new TextField("Senha");
+    TextField txtSenha = new TextField("Usuario");
+    TextField txtCargo = new TextField("Cargo");
+
+    Button btnSalvar = new Button("Salvar");
+
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        GridPane painel = new GridPane();
+        Scene scn = new Scene(painel,230,230);
+
+        Parent tela1 = FXMLLoader.load(getClass().getResource("Login"));
+
+
+        Label lblNome = new Label(" Nome");
+        Label lblCPF = new Label(" CPF");
+        Label lblTelefone = new Label(" Telefone");
+        Label lblEndereco = new Label(" Endereço");
+        Label lblUsuario = new Label(" Usuario");
+        Label lblSenha = new Label(" Senha");
+        Label lblCargo = new Label(" Cargo");
+
+        painel.add(lblNome , 0,1);
+        painel.add(lblCPF,0,2);
+        painel.add(lblTelefone, 0, 3);
+        painel.add(lblEndereco,0,4);
+        painel.add(lblUsuario,0,5);
+        painel.add(lblCargo,0,6);
+
+        painel.add(txtNome, 1,1);
+        painel.add(txtCPF,1,2);
+        painel.add(txtTelefone,1,3);
+        painel.add(txtEndereco,1,4);
+        painel.add(txtUsuario,1,5);
+        painel.add(txtSenha,1,6);
+        painel.add(txtCargo,1,7);
+
+        painel.add(btnSalvar,1,8);
+
+
+        stage.setScene(scn);
+        stage.setTitle("Cadastro funcionario");
+        stage.show();
+
+
+    }
+   // public static void main(String[] args) {Application.launch(args);}
+
+}
+
